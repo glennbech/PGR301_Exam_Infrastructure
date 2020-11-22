49b8bc07b30ec88e063146c2f1642b17346c2455
omamoh18 <you@example.com>
2020-11-22 22:19:08 +0100

    first commit

--- provider.tf
+++ provider.tf
@@ -2,12 +2,12 @@ terraform {
   backend "gcs" {
     bucket = "exampgr301bucket"
     prefix = "terraformstate"
-    credentials = "terraform-key.json"
+    credentials = "key-file.json"
   }
 }
 
 provider "google-beta" {
-  credentials = file("terraform-key.json")
+  credentials = file("key-file.json")
   project = "e-copilot-296419"
   version = "~> 3.0.0-beta.1"
 }
